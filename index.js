dbeaa39dc05bb69a41bc710394256fc6e2e97ac2
kazuyakurashima <gs.kaz224@gmail.com>
2025-07-28 01:01:40 +0900

    modify

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// 関数の定義
+
 const printWanko = () => {
     console.log("にんじゃわんこ");
   };
+
+// f() = console.log("にんじゃわんこ")
 
   const printHitsuji = () => {
     console.log("ひつじ仙人");
@@ -15,6 +19,14 @@ const printWanko = () => {
 // console.log("コールバック関数を呼び出します。")
 // callback()       g(x)
 
+
+
+// 関数の呼び出し
+printWanko();
+printHitsuji();
+
+
+
   // 引数をprintHitsujiに書き換えて出力を確認しましょう
   call(printWanko);
   call(printHitsuji);
@@ -22,3 +34,25 @@ const printWanko = () => {
   call( () => {
     console.log("たまご凡人");
   });
+
+
+
+
+
+
+
+
+
+  const introduce = (callback) => {
+    callback("ほんまかいな？")
+  };
+  
+  // introduce(callback)        f( g(x) )
+  // callback("ほんまかいな？")     g(x)
+  
+  introduce((name) => {
+    console.log(name);
+  });
+  
+  // f ( g(x) )
+  // g(x)            
