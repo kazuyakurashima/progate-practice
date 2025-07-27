47d9405c2b76c4867988df299a71f02cbf501c15
kazuyakurashima <gs.kaz224@gmail.com>
2025-07-28 00:20:56 +0900

    modify

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const printWanko = () => {
     console.log("にんじゃわんこ");
   };
-  
+
   const printHitsuji = () => {
     console.log("ひつじ仙人");
   };
-  
+
   const call = (callback) => {
     console.log("コールバック関数を呼び出します。");
     callback();
   };
-  
+
+// call(callback)   f( g(x) )
+// console.log("コールバック関数を呼び出します。")
+// callback()       g(x)
+
   // 引数をprintHitsujiに書き換えて出力を確認しましょう
   call(printWanko);
   call(printHitsuji);
 
-  
+  call( () => {
+    console.log("たまご凡人");
+  });
