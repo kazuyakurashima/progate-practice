86cc813d7b61673f6316bf1f2931ea014c407101
kazuyakurashima <gs.kaz224@gmail.com>
2025-07-28 01:42:30 +0900

    modify

--- index.js
+++ index.js
@@ -3,56 +3,60 @@
 const printWanko = () => {
     console.log("にんじゃわんこ");
   };
-
 // f() = console.log("にんじゃわんこ")
+
 
   const printHitsuji = () => {
     console.log("ひつじ仙人");
   };
+// f() = console.log("にんじゃわんこ")
+
 
   const call = (callback) => {
     console.log("コールバック関数を呼び出します。");
     callback();
   };
-
 // call(callback)   f( g(x) )
 // console.log("コールバック関数を呼び出します。")
-// callback()       g(x)
+// callback()       g()
+
+
+  const introduce = (callback) => {
+    callback("ほんまかいな？")
+  };
+  // introduce(callback)        f( g(x) )
+  // callback("ほんまかいな？")    g(x)
 
 
 
 // 関数の呼び出し
 printWanko();
 printHitsuji();
+// f()の実行
+  // にんじゃわんこ
+  // ひつじ仙人
 
 
-
-  // 引数をprintHitsujiに書き換えて出力を確認しましょう
   call(printWanko);
   call(printHitsuji);
+// f( g(x) )の実行
+  // clg("コールバック関数を呼び出します")
+  // printWanko();   にんじゃわんこ
+  // clg("コールバック関数を呼び出します")
+  // printWanko();   ひつじ仙人
 
   call( () => {
     console.log("たまご凡人");
   });
+// f( g(x) )の実行
+  // clg("コールバック関数を呼び出します")
+  // たまご凡人
 
 
-
-
-
-
-
-
-
-  const introduce = (callback) => {
-    callback("ほんまかいな？")
-  };
-  
-  // introduce(callback)        f( g(x) )
-  // callback("ほんまかいな？")     g(x)
-  
   introduce((name) => {
     console.log(name);
   });
-  
-  // f ( g(x) )
-  // g(x)            
+
+  // f ( g(x) )の実行
+  // g(x)の実行
+  // ほんまかいな
